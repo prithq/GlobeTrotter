bbbbb79799f21982c0e82760dfad1cfe7ff8240d
Prit Patel <projects.pritpatel@gmail.com>
2026-08-22 09:33:21 +0530

    initial structure

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+const app=express()
+
+
+app.use(express.json())
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
+app.get("/health",(req,res)=>{
+    res.send("working")
+})
+app.use(8000)
+
+export default app
